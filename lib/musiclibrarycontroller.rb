2a9c7ecee41ad52c2c50f99b1a8897ce11b47f9e
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-09 14:54:01 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -18,7 +18,7 @@ class MusicLibraryController
   def list_artists
     list_of_files = @new_music_library.files.collect { |file| file.split(" - ") }
     list_of_artists = list_of_files.collect { |array| array[0] }
-    list_of_artists.sort!.uniq 
+    list_of_artists.sort!
     list_of_artists.each_with_index do |artist, index|
       puts "#{index + 1}. #{artist}"
     end
